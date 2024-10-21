99399663ff74ec385378acd15789d0c7b9d041c6
vijaydigitech <vijaydigitech55@gmail.com>
2024-10-21 20:12:01 +0530

    New Development changes

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+    document.getElementById("text").innerText = "You clicked the button! The text has changed.";
+});
